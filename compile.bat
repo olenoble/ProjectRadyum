adadd36a6449c10930d33bd5a7572f590624450d
olenoble <olenoble@users.noreply.github.com>
2020-06-14 16:18:40 +0200

    some notes

--- compile.bat
+++ compile.bat
@@ -1,9 +1,13 @@
 @echo off
 set filename=radyum
 
-echo parameter zi allows for debug symbols
-C:\TASM\tasm.exe /zi /m3 %filename%.asm
-C:\TASM\tlink.exe /3 /x /v %filename%.obj
+rem parameter zi allows for debug symbols
+rem C:\TASM\tasm.exe /zi /m3 %filename%.asm
+rem C:\TASM\tlink.exe /3 /x /v %filename%.obj
+
+C:\TASM\tasm.exe /m3 %filename%.asm
+C:\TASM\tlink.exe /3 /x %filename%.obj
+
 
 REM %1.exe
 
