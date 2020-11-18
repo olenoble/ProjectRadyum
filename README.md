bd1b8ac6825e1dc488692d10bffa0b9c414ef899
olenoble <olenoble@users.noreply.github.com>
2020-11-18 21:36:18 +0100

    fixed QR code

--- README.md
+++ README.md
@@ -1,20 +1,20 @@
 # ProjectRadyum
 
-Credits<br><br>
-<ol>
-<li>Mention Jasmine<br>
-<li>Tools (incl. GUI TASM / TD, Grafx, dp2e, wex editor, piskel, etc...)<br>
-<li>How many lines of code ?<br>
-<li>discuss synthwave<br>
-<li>discuss project evolution and reflection in hindsight<br>
-<li>add ref to https://open.spotify.com/playlist/5jXZCjqxnjHr4EzJneGc0T <br>
-<li>Also sorry for the typos in the blog<br>
-<li>explain the origin of midnightoilasm<br>
-</ol>
 
 
 TO DO (REQUIRED)<br><br>
 <ol>
-<li><b>Change QR code to point to gift page<b>. Create blog page for gift for winner (with encrypted zipped up data)<br>
+<li>Add gifts to winning page<br>
+<li>Credits:<br><br>
+    <ol>
+    <li>Mention Jasmine<br>
+    <li>Tools (incl. GUI TASM / TD, Grafx, dp2e, wex editor, piskel, etc...)<br>
+    <li>How many lines of code ?<br>
+    <li>discuss synthwave<br>
+    <li>discuss project evolution and reflection in hindsight<br>
+    <li>add ref to https://open.spotify.com/playlist/5jXZCjqxnjHr4EzJneGc0T <br>
+    <li>Also sorry for the typos in the blog<br>
+    <li>explain the origin of midnightoilasm<br>
+    </ol>
 </ol>
 
--- winner.asm
+++ winner.asm
@@ -5,21 +5,23 @@
 .DATA
 WINNING_MSG     db "Felicitations - Vous avez atteint la sortie de mon escape game facon 80s.", 13, 10
                 db "Vous avez resolu tous les puzzles et merite votre recompense:", 13, 10, 13, 10
-                db 020h, 020h, 020h, 0dch, 0dch, 0dch, 0dch, 0dch, 0dch, 0dch, 020h, 020h, 0dch, 020h, 020h, 0dch, 0dch, 0dch, 020h, 0dch, 0dch, 0dch, 0dch, 0dch, 020h, 0dch, 0dch, 0dch, 0dch, 0dch, 0dch, 0dch, 020h, 020h, 13, 10
-                db 020h, 020h, 020h, 0dbh, 020h, 0dch, 0dch, 0dch, 020h, 0dbh, 020h, 0dbh, 0dch, 0dch, 0dbh, 0dbh, 0dbh, 0dfh, 0dbh, 020h, 0dch, 0dbh, 020h, 0dbh, 020h, 0dbh, 020h, 0dch, 0dch, 0dch, 020h, 0dbh, 020h, 020h, 13, 10
-                db 020h, 020h, 020h, 0dbh, 020h, 0dbh, 0dbh, 0dbh, 020h, 0dbh, 020h, 0dch, 0dfh, 0dfh, 0dfh, 020h, 0dbh, 0dbh, 0dch, 020h, 020h, 0dbh, 020h, 020h, 020h, 0dbh, 020h, 0dbh, 0dbh, 0dbh, 020h, 0dbh, 020h, 020h, 13, 10
-                db 020h, 020h, 020h, 0dbh, 0dch, 0dch, 0dch, 0dch, 0dch, 0dbh, 020h, 0dbh, 0dfh, 0dch, 0dfh, 0dch, 0dfh, 0dbh, 0dfh, 0dbh, 0dfh, 0dch, 0dfh, 0dch, 020h, 0dbh, 0dch, 0dch, 0dch, 0dch, 0dch, 0dbh, 020h, 020h, 13, 10
-                db 020h, 020h, 020h, 0dch, 0dch, 020h, 0dch, 020h, 020h, 0dch, 0dch, 0dfh, 0dfh, 0dch, 020h, 0dfh, 020h, 0dfh, 0dbh, 0dch, 0dfh, 020h, 0dfh, 0dch, 020h, 0dch, 0dch, 0dch, 020h, 0dch, 0dch, 020h, 020h, 020h, 13, 10
-                db 020h, 020h, 020h, 020h, 0dch, 0dbh, 0dch, 0dch, 0dbh, 0dch, 0dfh, 0dch, 0dch, 0dbh, 0dfh, 020h, 020h, 0dfh, 020h, 0dbh, 020h, 020h, 0dch, 0dfh, 0dfh, 0dbh, 020h, 020h, 0dbh, 0dch, 0dch, 0dfh, 020h, 020h, 13, 10
-                db 020h, 020h, 020h, 0dfh, 0dbh, 020h, 0dbh, 0dbh, 0dbh, 0dch, 0dbh, 0dbh, 020h, 020h, 020h, 0dch, 020h, 0dfh, 0dch, 0dbh, 0dch, 020h, 0dfh, 0dbh, 0dch, 0dch, 0dfh, 0dfh, 0dch, 0dfh, 0dbh, 0dch, 020h, 020h, 13, 10
-                db 020h, 020h, 020h, 0dfh, 0dfh, 0dbh, 0dfh, 0dch, 020h, 0dch, 020h, 0dbh, 0dch, 0dbh, 0dfh, 0dbh, 020h, 020h, 020h, 020h, 020h, 020h, 0dfh, 0dfh, 0dbh, 0dch, 0dch, 0dch, 0dch, 0dch, 0dch, 0dch, 020h, 020h, 13, 10
-                db 020h, 020h, 020h, 0dch, 0dbh, 0dch, 0dbh, 0dbh, 020h, 0dch, 0dbh, 020h, 0dbh, 0dch, 0dch, 0dfh, 0dbh, 0dbh, 020h, 020h, 0dbh, 0dfh, 0dfh, 0dbh, 0dch, 0dfh, 020h, 020h, 0dfh, 020h, 0dbh, 020h, 020h, 020h, 13, 10
-                db 020h, 020h, 020h, 0dch, 020h, 0dbh, 0dch, 0dch, 0dfh, 0dch, 0dbh, 020h, 0dfh, 0dch, 020h, 020h, 0dch, 020h, 020h, 0dbh, 0dch, 020h, 020h, 020h, 0dfh, 0dfh, 0dbh, 0dch, 0dfh, 020h, 0dch, 0dbh, 020h, 020h, 13, 10
-                db 020h, 020h, 020h, 0dch, 020h, 0dfh, 0dbh, 0dch, 0dch, 0dch, 0dfh, 0dfh, 0dbh, 020h, 0dbh, 0dch, 0dfh, 0dch, 0dch, 0dch, 0dfh, 0dfh, 0dch, 0dch, 0dbh, 0dbh, 0dbh, 0dbh, 020h, 0dch, 0dfh, 0dfh, 020h, 020h, 13, 10
-                db 020h, 020h, 020h, 0dch, 0dch, 0dch, 0dch, 0dch, 0dch, 0dch, 020h, 0dbh, 0dch, 0dbh, 0dbh, 0dbh, 0dbh, 0dch, 020h, 020h, 0dbh, 0dbh, 0dfh, 0dbh, 020h, 0dch, 020h, 0dbh, 020h, 0dfh, 0dbh, 0dfh, 020h, 020h, 13, 10
-                db 020h, 020h, 020h, 0dbh, 020h, 0dch, 0dch, 0dch, 020h, 0dbh, 020h, 020h, 0dfh, 0dch, 0dbh, 0dfh, 0dfh, 0dch, 0dfh, 0dfh, 0dch, 0dfh, 020h, 0dbh, 0dch, 0dch, 0dch, 0dbh, 0dfh, 0dfh, 0dbh, 0dch, 020h, 020h, 13, 10
-                db 020h, 020h, 020h, 0dbh, 020h, 0dbh, 0dbh, 0dbh, 020h, 0dbh, 020h, 0dfh, 0dbh, 0dfh, 0dbh, 020h, 0dbh, 0dbh, 020h, 020h, 0dbh, 0dbh, 0dbh, 0dbh, 0dch, 0dfh, 0dfh, 0dbh, 0dbh, 0dbh, 0dfh, 0dch, 020h, 020h, 13, 10
-                db 020h, 020h, 020h, 0dbh, 0dch, 0dch, 0dch, 0dch, 0dch, 0dbh, 020h, 0dbh, 0dbh, 0dch, 0dfh, 0dch, 0dch, 020h, 0dch, 0dfh, 0dbh, 020h, 0dfh, 0dbh, 020h, 020h, 0dch, 020h, 0dch, 020h, 0dbh, 020h, 020h, 020h, 13, 10
+                db 020h, 0dch, 0dch, 0dch, 0dch, 0dch, 0dch, 0dch, 020h, 020h, 0dch, 020h, 020h, 020h, 020h, 0dch, 020h, 0dch, 0dch, 0dch, 0dch, 020h, 0dch, 0dch, 0dch, 020h, 020h, 0dch, 0dch, 0dch, 0dch, 0dch, 0dch, 0dch, 13, 10
+                db 020h, 0dbh, 020h, 0dch, 0dch, 0dch, 020h, 0dbh, 020h, 0dfh, 0dbh, 0dch, 0dbh, 0dfh, 020h, 0dfh, 0dbh, 020h, 0dfh, 020h, 020h, 0dfh, 0dch, 0dch, 0dch, 020h, 020h, 0dbh, 020h, 0dch, 0dch, 0dch, 020h, 0dbh, 13, 10
+                db 020h, 0dbh, 020h, 0dbh, 0dbh, 0dbh, 020h, 0dbh, 020h, 0dfh, 0dfh, 0dch, 0dfh, 0dch, 0dbh, 0dfh, 020h, 0dch, 0dfh, 020h, 0dch, 0dfh, 0dch, 0dbh, 0dch, 0dfh, 020h, 0dbh, 020h, 0dbh, 0dbh, 0dbh, 020h, 0dbh, 13, 10
+                db 020h, 0dbh, 0dch, 0dch, 0dch, 0dch, 0dch, 0dbh, 020h, 0dbh, 020h, 0dbh, 020h, 0dch, 020h, 0dbh, 0dfh, 0dch, 0dfh, 0dch, 020h, 0dbh, 0dfh, 0dch, 020h, 0dch, 020h, 0dbh, 0dch, 0dch, 0dch, 0dch, 0dch, 0dbh, 13, 10
+                db 020h, 0dch, 0dch, 0dch, 0dch, 0dch, 020h, 0dch, 0dch, 0dch, 0dch, 0dch, 020h, 0dbh, 0dch, 0dbh, 020h, 0dfh, 0dch, 0dfh, 020h, 0dch, 0dfh, 0dch, 0dbh, 0dch, 0dch, 020h, 0dch, 020h, 0dch, 020h, 0dch, 020h, 13, 10
+                db 020h, 0dfh, 020h, 020h, 0dfh, 0dfh, 0dch, 0dch, 0dbh, 0dfh, 0dbh, 020h, 0dch, 0dch, 0dfh, 0dch, 0dch, 0dch, 0dbh, 0dfh, 0dfh, 0dch, 020h, 0dch, 0dfh, 0dch, 0dbh, 0dfh, 0dch, 020h, 0dch, 0dfh, 0dbh, 0dfh, 13, 10
+                db 020h, 0dfh, 0dbh, 0dfh, 0dbh, 0dfh, 0dch, 0dch, 020h, 0dfh, 0dbh, 0dfh, 0dbh, 020h, 020h, 020h, 0dfh, 020h, 0dch, 020h, 0dbh, 0dfh, 0dfh, 0dbh, 0dch, 0dch, 0dch, 020h, 0dbh, 0dbh, 0dch, 0dfh, 020h, 020h, 13, 10
+                db 020h, 0dch, 020h, 0dfh, 0dbh, 0dfh, 0dbh, 0dch, 0dbh, 0dbh, 0dfh, 0dfh, 020h, 0dfh, 0dbh, 0dch, 0dch, 0dbh, 020h, 020h, 0dfh, 020h, 0dbh, 0dbh, 0dfh, 0dch, 0dch, 0dbh, 020h, 0dch, 020h, 020h, 0dbh, 0dfh, 13, 10
+                db 020h, 020h, 0dfh, 0dfh, 0dbh, 0dfh, 0dch, 0dch, 020h, 020h, 0dfh, 0dch, 020h, 0dbh, 0dfh, 0dch, 0dch, 020h, 0dch, 020h, 020h, 0dch, 0dfh, 020h, 0dfh, 0dbh, 0dbh, 0dfh, 0dch, 0dch, 020h, 0dfh, 0dch, 020h, 13, 10
+                db 020h, 0dbh, 0dfh, 0dch, 0dch, 0dfh, 020h, 0dch, 020h, 0dfh, 0dfh, 0dch, 0dch, 0dch, 020h, 020h, 0dch, 0dbh, 0dch, 0dch, 0dfh, 020h, 020h, 0dch, 0dfh, 0dch, 020h, 0dbh, 020h, 020h, 0dbh, 020h, 0dbh, 0dfh, 13, 10
+                db 020h, 0dfh, 0dch, 0dbh, 0dch, 0dfh, 020h, 0dch, 0dbh, 0dfh, 0dfh, 020h, 0dbh, 020h, 020h, 0dfh, 0dfh, 020h, 0dch, 0dch, 0dfh, 0dch, 0dfh, 0dch, 0dbh, 0dch, 0dbh, 020h, 0dbh, 0dbh, 020h, 0dfh, 0dch, 020h, 13, 10
+                db 020h, 0dbh, 0dfh, 0dbh, 0dbh, 020h, 0dch, 0dch, 0dbh, 020h, 0dbh, 0dbh, 020h, 0dfh, 0dbh, 0dbh, 0dch, 0dbh, 0dfh, 0dfh, 0dfh, 020h, 0dfh, 020h, 0dfh, 0dfh, 0dch, 0dbh, 0dch, 0dch, 0dbh, 020h, 0dbh, 0dfh, 13, 10
+                db 020h, 0dbh, 020h, 020h, 020h, 020h, 0dch, 0dch, 0dfh, 0dch, 0dfh, 0dfh, 020h, 0dbh, 0dfh, 0dch, 0dfh, 0dfh, 0dch, 020h, 020h, 0dch, 0dfh, 0dbh, 0dch, 0dch, 0dch, 0dch, 0dch, 0dbh, 0dfh, 0dfh, 020h, 0dch, 13, 10
+                db 020h, 0dch, 0dch, 0dch, 0dch, 0dch, 0dch, 0dch, 020h, 0dbh, 0dch, 020h, 0dch, 0dch, 020h, 020h, 0dch, 0dch, 020h, 0dfh, 0dfh, 020h, 020h, 0dch, 0dch, 0dbh, 020h, 0dch, 020h, 0dbh, 0dbh, 0dch, 0dch, 0dfh, 13, 10
+                db 020h, 0dbh, 020h, 0dch, 0dch, 0dch, 020h, 0dbh, 020h, 0dch, 0dch, 0dfh, 0dbh, 020h, 020h, 0dfh, 0dch, 0dch, 0dch, 020h, 0dfh, 0dch, 0dfh, 0dbh, 0dfh, 0dbh, 0dch, 0dch, 0dch, 0dbh, 0dfh, 0dfh, 0dch, 0dbh, 13, 10
+                db 020h, 0dbh, 020h, 0dbh, 0dbh, 0dbh, 020h, 0dbh, 020h, 0dbh, 0dfh, 0dbh, 020h, 0dfh, 0dbh, 0dbh, 0dch, 0dbh, 020h, 020h, 0dfh, 0dch, 0dch, 0dch, 0dfh, 0dch, 0dfh, 0dch, 0dfh, 0dfh, 0dch, 0dch, 0dfh, 0dfh, 13, 10
+                db 020h, 0dbh, 0dch, 0dch, 0dch, 0dch, 0dch, 0dbh, 020h, 0dbh, 0dbh, 0dch, 020h, 0dbh, 0dfh, 0dch, 0dbh, 0dfh, 0dch, 0dfh, 0dbh, 0dfh, 0dfh, 0dfh, 0dbh, 0dch, 0dch, 020h, 0dfh, 020h, 0dfh, 0dfh, 0dch, 020h, 13, 10
                 db 13, 10, 13, 10, "$"
 WINNING_CODE    db "Votre code pour obtenir votre recompense est ", "$"
 PLAYER1_CODE    db "Anthony4Ever!", 13, 10, "$"
